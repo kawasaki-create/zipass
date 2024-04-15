82f95144fdfe7d4df594da459e5791f781138d9a
kawasaki <r.kawasaki@leafnet.jp>
2024-04-15 09:35:03 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/kawasaki_create/zipass/zipass/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kawasaki_create.zipass.zipass
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/root.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RootWidgets extends StatefulWidget {
+  const RootWidgets({super.key});
+
+  @override
+  State<RootWidgets> createState() => _RootWidgetsState();
+}
+
+class _RootWidgetsState extends State<RootWidgets> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
